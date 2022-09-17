10bc45889442861a28a96c68aaa951efb1cb2baa
crispycret <bnadeau.dev@gmail.com>
2022-09-17 13:46:58 -0700

    SierpinskiDrawer successfully creates the Sierpinski Triangle. Next Complete the SierpinskiSettings allowing the user to pass custom settings to the SierpinskiDrawer.

--- app/src/main/java/com/example/fractalengine/canvas/SierpinskiDrawer.java
+++ app/src/main/java/com/example/fractalengine/canvas/SierpinskiDrawer.java
@@ -8,15 +8,16 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.View;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class SierpinskiDrawer extends FractalDrawer {
 
-    public int DEFAULT_MAX_DEPTH = 1;
-    public int DEFAILT_STROKE_WIDTH = 3;
+    public int DEFAULT_MAX_DEPTH = 3;
+    public int DEFAILT_STROKE_WIDTH = 7;
 
-    public int depth = 1;
-    public int strokeWidth = 3;
+    public int depth = 3;
+    public int strokeWidth = 7;
 
     public SierpinskiDrawer(SurfaceHolder surfaceHolder, Bundle bundle) {
         super(surfaceHolder, bundle);
@@ -51,102 +52,86 @@ public class SierpinskiDrawer extends FractalDrawer {
         Paint paint = new Paint();
         paint.setAntiAlias(true);
         paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setStrokeWidth(strokeWidth);
 
         int width = canvas.getWidth();
         int height = canvas.getHeight();
 
-        double offsetX = 0.025;
-        double offsetY = 0.05;
-        int startX = (int)(width * offsetX);;
-        int startY = height - (int)(height * offsetY * 2);
-        double sidelen = width - (width * offsetX * 2);
-        Integer[] pos = new Integer[] {startX, startY};
+        float offsetX = 0.025f;
+        float offsetY = 0.05f;
+        float startX = width * offsetX;;
+        float startY = height - (height * offsetY * 2);
+        float sidelen = width - (width * offsetX * 2);
+        Float[] pos = new Float[] {startX, startY};
 
-        paint.setStrokeWidth(strokeWidth);
-        Log.w("Sierpinski: ", "Start!");
-        createSierpinskiTriangle(pos, sidelen, depth, canvas, paint);
-        Log.w("Sierpinski: ", "Done!");
-
-//        canvas.drawLine(
-//            0, canvas.getHeight(),
-//            canvas.getWidth(), 0, paint
-//        );
+        createSierpinskiTriangle(pos, sidelen, 1, canvas, paint);
 
     }
-//
-//    public int[] createTriangleLines (int startX, int startY, double sidelen) {
-//        // Each triangle is made of three lines
-//        // Each Line consits of a start and end point.
-//        // Each Triangle is made of 3 lines.
-//
-//        ArrayList<Integer[]> lines = new ArrayList<Integer[]>();
-//
-//        int endX, endY;
-//        lines.add(startX, startY, endX, endY);
-//
-//        // Modify startX, startY, endX, endY
-//        lines.add(startX, startY, endX, endY);
-//
-//        // Modify startX, startY, endX, endY
-//        lines.add(startX, startY, endX, endY);
-//
-//
-//    }
+
+
 
 
     public void createSierpinskiTriangle(
-            Integer[] pos, double sidelen, int depth,
-            Canvas canvas, Paint paint
-            ) {
+        Float[] pos, float sidelen, int depth, Canvas canvas, Paint paint
+    ) {
 
-        double innerTriangleSidelen = sidelen / 2; // side length of inner triangles is half the side length of the outer triangle
-        ArrayList<Integer[]> innerTrianglesPositions = new ArrayList<Integer[]>();
+        Log.w("Depth: ", String.valueOf(depth));
+        float innerTriangleSidelen = sidelen / 2; // side length of inner triangles is half the side length of the outer triangle
+        ArrayList<Float[]> innerTrianglePositions = new ArrayList<Float[]>();
 
-
-        // Add first three positions
-        innerTrianglesPositions.add(pos);
-        innerTrianglesPositions.add(new Integer[]{
-                pos[0] + (int)innerTriangleSidelen, pos[1]
+        /*
+         Add the first 3 triangle positions to start from.
+         These 3 points create a triangle.
+         These positions are the same as what was used in the createTriangle function
+        */
+        innerTrianglePositions.add(pos);
+        innerTrianglePositions.add(new Float[] {
+                pos[0] + innerTriangleSidelen,
+                pos[1]
         });
-        innerTrianglesPositions.add(new Integer[]{
-                pos[0] + (int)(innerTriangleSidelen / 2),
-                pos[1] - (int)(Math.sin(Math.PI / 3) * innerTriangleSidelen)
+        innerTrianglePositions.add(new Float[] {
+                pos[0] + innerTriangleSidelen / 2,
+                (float) (pos[1] - Math.sin(Math.PI / 3) * innerTriangleSidelen)
         });
+
 
 
         if(depth == 0) {
-            for (Integer[] trianglePosition : innerTrianglesPositions) {
+            for (Float[] trianglePosition : innerTrianglePositions) {
                 createTriangle(trianglePosition, innerTriangleSidelen, canvas, paint);
             }
         } else {
-            for (Integer[] trianglePosition : innerTrianglesPositions) {
-                createSierpinskiTriangle(trianglePosition, innerTriangleSidelen, depth - 1, canvas, paint);
+            for (Float[] trianglePosition : innerTrianglePositions) {
+                createSierpinskiTriangle(
+                    trianglePosition, innerTriangleSidelen, depth - 1, canvas, paint
+                );
             }
         }
 
 
     }
 
-    public void createTriangle (Integer[] pos, double sidelen, Canvas ctx, Paint paint) {
+    public void createTriangle (Float[] pos, double sidelen, Canvas ctx, Paint paint) {
 
-//        ctx.strokeStyle = color
+        float startX, startY, endX, endY;
 
         // note that (0,0) in canvas is the top left, so 'up' on the vertical component would use substraction.
         // draw line from left vertex to top vertex
-        float x0 = pos[0];
-        float y0 = pos[1];
-        float x1 = (float) (pos[0] + sidelen / 2);
-        float y1 = (float) (pos[1] - sidelen * Math.sin(Math.PI/3));
-        ctx.drawLine(x0,y0, x1, y1, paint);
-
-        // draw line from left vertex to top vertex
-        float x2 = (float) (pos[0] + sidelen / 2);
-        float y2 = (float) (pos[1] - sidelen * Math.sin(Math.PI/3));
-        ctx.drawLine(x1, y1, x2, y2, paint);
+        startX = pos[0]; startY = pos[1];
+        endX = (float) (pos[0] + sidelen / 2);
+        endY = (float) (pos[1] - sidelen * Math.sin(Math.PI/3));
+        ctx.drawLine(startX, startY, endX, endY, paint);
 
         // draw line from top vertex to right vertex
-        float x3 = (float) (pos[0] + sidelen);
-        float y3 = pos[1];
-        ctx.drawLine(x2, y2, x3, y3, paint);
+        startX = endX; startY = endY;
+        endX = (float) (pos[0] + sidelen);
+        endY = pos[1];
+        ctx.drawLine(startX, startY, endX, endY, paint);
+
+        // draw line from right vertex back to left vertex
+        startX = endX; startY = endY;
+        endX = pos[0];endY = pos[1];
+        ctx.drawLine(startX, startY, endX, endY, paint);
     }
 }
